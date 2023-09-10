5f4cd4833ce91bf782f2e95585a14aed6300e572
GarimaUttam <garimauttam191203@gmail.com>
2023-09-11 01:03:06 +0530

    Completed the companies_logo section

--- real_eastate_project/src/App.jsx
+++ real_eastate_project/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css";
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
     <div className="App">
-      
+      <div>
       <Header/>
       <Hero/>
-  
+      </div>
+     
+     <Companies/>
     </div>
   );
 }
---
+++ real_eastate_project/src/components/Companies/Companies.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Companies.css";
+
+const Companies = () => {
+    return(
+    <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            <img src="./c_logo1.png" alt=""/>
+            <img src="./c_logo2.png" alt=""/>
+            <img src="./c_logo3.png" alt=""/>
+            <img src="./c_logo4.png" alt=""/>
+            <img src="./c_logo5.jpeg" alt=""/>
+
+            </div>
+    </section>
+    )
+}
+
+export default Companies;
