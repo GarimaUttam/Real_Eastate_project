87192115458858a3d79d458ef07aa6f2a8bf5fa1
GarimaUttam <garimauttam191203@gmail.com>
2023-09-10 19:33:15 +0530

    Added Hero Section to it

--- real_eastate_project/src/components/Hero/Hero.jsx
+++ real_eastate_project/src/components/Hero/Hero.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Hero.css";
+import {HiLocationMarker} from "react-icons/";
+
 const Hero = () =>{
     return (
         <section className="hero-wrapper">
-            <div className="paddings innerwidth flexCenter hero-center">
+            <div className="paddings innerWidth flexCenter hero-container">
 
                 {/* left */}
                 <div className="flexColStart hero-left">
                     <div className="hero-title">
+                        <div className="orange-circle">
+                        </div>
                         <h1>
                             Discover <br/>
                             Your Comfort <br/>
@@ -16,11 +20,18 @@ const Hero = () =>{
                     </div>
 
                     <div className="flexColStart hero-des">
-                        <span>Find a variety of properties that suits you</span>
-                        <span>Zero stress in finding your dream house</span>
+                        <ul>
+                        <li><span>Find a variety of properties that suits you</span>
+                        </li>
+                        <li><span>Zero stress in finding your dream house</span>
+                        </li>
+                        </ul>
                     </div>
-                    <div className="search-bar">
-                        Search bar
+
+                    <div className="flexCenter search-bar">
+                        <i class="fa-solid fa-location-dot" color="blue" size={25}></i>
+                        <input type="text" placeholder="find your home"></input>
+                        <button className="button">Search</button>
                     </div>
                 </div>
 
